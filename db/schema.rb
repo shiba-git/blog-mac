f72a4e6cc7e22110699f367bafb22f89cf900fcd
shiba-git <shiba.webbusiness@gmail.com>
2016-08-04 22:58:29 +0900

    finish user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804043919) do
+ActiveRecord::Schema.define(version: 20160804123408) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20160804043919) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
 end
